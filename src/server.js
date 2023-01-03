ada293e4c1e5a0833cbd5b2a83a6a956e6887c80
maivankien <maivankien1710@gmail.com>
2023-01-03 10:48:12 +0700

    CRUD SQL

--- src/server.js
+++ src/server.js
@@ -2,36 +2,21 @@ require('dotenv').config()
 const express = require('express')
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
-const mysql = require('mysql2')
+const connection = require('./config/database')
 
 const app = express()
 const port = process.env.PORT || 3000
 const hostname = process.env.HOST_NAME
+
+// config req.body
+app.use(express.json()) // for json
+app.use(express.urlencoded({ extended: true }))
 
 // Config viewEngine
 configViewEngine(app)
 
 // Khai báo route
 app.use('/', webRoutes)
-
-// Test connection
-// create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307, // default 3306
-    user: 'root', 
-    password: '123456', // default empty
-    database: 'maivankien'
-  })
-
-// simple query
-connection.query(
-    'SELECT * FROM Users u',
-    function(err, results, fields) {
-      console.log("results: ", results); // results contains rows returned by server
-      console.log("fields: ", fields); // fields contains extra meta data about results, if available
-    }
-  )
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
